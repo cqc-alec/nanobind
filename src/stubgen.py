ba7fc543a5cf66d9c786d9df5b3264a660b79ba3
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-11 09:06:02 +0100

    Add `private_exceptions` argument.

--- src/stubgen.py
+++ src/stubgen.py
@@ -173,6 +173,7 @@ class StubGen:
         recursive: bool = False,
         include_docstrings: bool = True,
         include_private: bool = False,
+        private_exceptions: set[str] = set(),
         include_internal_imports: bool = True,
         include_external_imports: bool = False,
         max_expr_length: int = 50,
@@ -191,6 +192,9 @@ class StubGen:
 
         # Include private members that start or end with a single underscore?
         self.include_private = include_private
+
+        # Private members that should be included regardless of `include_private`.
+        self.private_exceptions = private_exceptions
 
         # Include types and functions imported from the same package (but a different module)
         self.include_internal_imports = include_internal_imports
@@ -794,6 +798,7 @@ class StubGen:
                     (name[0] == "_" and name[1] != "_")
                     or (name[-1] == "_" and name[-2] != "_")
                 )
+                and name not in self.private_exceptions
             ):
                 return
 
